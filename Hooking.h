ec456245f3193fb2f7d54136553b2daac278d09b
AlSharji0 <asharji1828@gmail.com>
2024-09-05 19:27:24 +0400

    .

--- Hooking.h
+++ Hooking.h
@@ -5,8 +5,6 @@
 typedef HANDLE (WINAPI *FindFirstFileA_t)(LPCSTR, LPWIN32_FIND_DATAA);
 typedef BOOL(WINAPI* FindNextFileA_t)(HANDLE, LPWIN32_FIND_DATAA);
 
-extern FindFirstFileA_t OriginalFFF;
-extern FindNextFileA_t OriginalFNF;
 extern char FileName[];
 
 HANDLE WINAPI HookedFFF(LPCSTR lpFileName, LPWIN32_FIND_DATAA lpFindData);
